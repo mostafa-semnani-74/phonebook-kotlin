7b717b8d7ce3b310e8d2f334a4a1356472f3aa64
Mostafa Semnani <mostafa.semnani.74@gmail.com>
2023-08-25 14:44:10 +0330

    init project

---
+++ src/main/kotlin/ir/mostafa/semnani/phonebookkotlin/PhonebookKotlinApplication.kt
@@ -0,0 +1,11 @@
+package ir.mostafa.semnani.phonebookkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PhonebookKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<PhonebookKotlinApplication>(*args)
+}
